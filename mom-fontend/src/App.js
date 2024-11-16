6b278ee5ca7c257dbb3055e7c59a1b6cc9782cdd
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2024-11-16 18:15:12 +0530

    AuthGuard added

--- mom-fontend/src/App.js
+++ mom-fontend/src/App.js
@@ -1,44 +1,51 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import Login from './component/Login/Login';
 import ForgotPassword from './component/forgot-password/ForgotPassword';
 import Signin from './component/sign-in-OTP/Signin';
 import OTPVerification from './component/Verify-Otp/Verify-Otp';
 import Dashboard from './component/Dashboard/Dashboard';
-import 'react-toastify/dist/ReactToastify.css';
 import SignUp from './component/SignUp/SignUp';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider } from './context/AuthContext';
+import AuthGuard from './component/AuthGuard';
 import './App.css';
 
 function App() {
-
-
   return (
-    <Router>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      
-     
+    <AuthProvider>
+      <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/verify-otp" element={<OTPVerification />} />
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/forgot-password" element={<ForgotPassword />}/>
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route
+            path="/dashboard"
+            element={
+              <AuthGuard>
+                <Dashboard />
+              </AuthGuard>
+            }
+          />
+          <Route path="/verify-otp" element={<OTPVerification />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
